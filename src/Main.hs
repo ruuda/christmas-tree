cb48e4944ac7d1f0170532289249900df1ce55be
Ruud van Asseldonk <dev@veniogames.com>
2016-12-04 23:43:19 +0100

    Implement rainbow color schemes

--- src/Main.hs
+++ src/Main.hs
@@ -11,6 +11,7 @@ module Main where
 
 import Control.Monad (forever)
 import Data.Bits (xor)
+import Data.Fixed (mod')
 import Data.Monoid ((<>))
 import Data.Word (Word8)
 import System.Environment (getArgs)
@@ -20,6 +21,7 @@ import System.Hardware.Serialport
 import qualified Data.ByteString as ByteString
 import qualified Data.ByteString.Lazy as ByteString
 import qualified Data.ByteString.Builder as ByteString
+import qualified Data.Time.Clock as Clock
 
 import Debug.Trace (traceShow)
 
@@ -28,12 +30,48 @@ type Color = (Float, Float, Float)
 mapColor :: (Float -> a) -> Color -> (a, a, a)
 mapColor f (r, g, b) = (f r, f g, f b)
 
+-- Create a color from hue, saturation, and lightness. All values are in the
+-- range 0-1.
+hsl :: Float -> Float -> Float -> Color
+hsl h s l = (r + m, g + m, b + m)
+  where
+    wrap x = x - (fromInteger (floor x))
+    h' = h `mod'` 1
+    c = s * (1 - abs (2 * l - 1))
+    x = c * (1 - abs (((h * 6) `mod'` 2) - 1))
+    m = l - 0.5 * c
+    (r, g, b) | h' < 1/6 = (c, x, 0)
+              | h' < 2/6 = (x, c, 0)
+              | h' < 3/6 = (0, c, x)
+              | h' < 4/6 = (0, x, c)
+              | h' < 5/6 = (x, 0, c)
+              | otherwise = (c, 0, x)
+
+data Mode
+  = Rainbow
+  | RainbowCycle
+
+-- Compute the color for each of the 25 LEDs, given the time and mode.
+assignColors :: Float -> Mode -> [Color]
+assignColors t mode = fmap f [1..25]
+  where
+    f i = case mode of
+      Rainbow ->
+        hsl (0.1 * fromIntegral i) 1.0 0.5
+      RainbowCycle ->
+        hsl (t * 0.05 + 0.1 * fromIntegral i) 1.0 0.5
+
+getSecondsSinceMidnight :: IO Float
+getSecondsSinceMidnight = fmap toSecs Clock.getCurrentTime
+  where
+    toSecs = (* 1.0e-12) . fromIntegral . Clock.diffTimeToPicoseconds . Clock.utctDayTime
+
 serializeColor :: Color -> ByteString.Builder
 serializeColor color =
   let
-    -- The LEDs have quite a non-linear response, so square the values to
+    -- The LEDs have quite a non-linear response, so cube the values to
     -- counter that a bit.
-    correctPower x = x * x
+    correctPower x = x * x * x
     toByte x = round (255.0 * x)
     (r, g, b) = mapColor (ByteString.word8 . toByte . correctPower) color
   in
@@ -75,12 +113,5 @@ main = do
   portAddr <- parseCommandLine
   sp <- openSerial portAddr settings
   forever $ do
-    putStrLn "Setting color ..."
-    let
-      colors = take 25 $ cycle [
-        (1.0, 0.0, 0.0),
-        (1.0, 1.0, 0.0),
-        (0.0, 1.0, 0.0),
-        (0.0, 1.0, 1.0),
-        (0.0, 0.0, 1.0)]
-    sendDatagram sp $ buildDatagram colors
+    t <- getSecondsSinceMidnight
+    sendDatagram sp $ buildDatagram (assignColors t RainbowCycle)
